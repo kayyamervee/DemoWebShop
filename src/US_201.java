fdb8f009e1f9dd760b95978ce3e34e596509fd66
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-09-16 17:43:12 +0300

    201 numaralı user story düzenlenmiştir.

--- src/US_201.java
+++ src/US_201.java
@@ -20,37 +20,38 @@ public class US_201 extends BaseDriver {
 
         driver.get("https://demowebshop.tricentis.com/");
         MyFunc.Wait(2);
+        Actions actions=new Actions(driver);
 
         WebElement registerButton=driver.findElement(By.xpath("(//div[@class='header-links']//a)[1]"));
-        new Actions(driver).moveToElement(registerButton).click().build().perform();
+        actions.moveToElement(registerButton).click().build().perform();
         MyFunc.Wait(2);
 
         WebElement genderMale=driver.findElement(By.xpath("(//div[@class='gender']//input)[1]"));
-        new Actions(driver).moveToElement(genderMale).click().build().perform();
+        actions.moveToElement(genderMale).click().build().perform();
         MyFunc.Wait(2);
 
         WebElement firstName=driver.findElement(By.cssSelector("[name='FirstName']"));
-        new Actions(driver).moveToElement(firstName).click().sendKeys(firstname).build().perform();
+        actions.moveToElement(firstName).click().sendKeys(firstname).build().perform();
         MyFunc.Wait(2);
 
         WebElement lastName=driver.findElement(By.cssSelector("[name='LastName']"));
-        new Actions(driver).moveToElement(lastName).click().sendKeys(lastname).build().perform();
+        actions.moveToElement(lastName).click().sendKeys(lastname).build().perform();
         MyFunc.Wait(2);
 
         WebElement emailButton=driver.findElement(By.cssSelector("[name='Email']"));
-        new Actions(driver).moveToElement(emailButton).click().sendKeys(email).build().perform();
+        actions.moveToElement(emailButton).click().sendKeys(email).build().perform();
         MyFunc.Wait(2);
 
         WebElement passwordButton=driver.findElement(By.xpath("(//input[@type='password'])[1]"));
-        new Actions(driver).moveToElement(passwordButton).click().sendKeys(password).build().perform();
+        actions.moveToElement(passwordButton).click().sendKeys(password).build().perform();
         MyFunc.Wait(2);
 
         WebElement confirmPassword=driver.findElement(By.cssSelector("[name='ConfirmPassword']"));
-        new Actions(driver).moveToElement(confirmPassword).click().sendKeys(password).build().perform();
+        actions.moveToElement(confirmPassword).click().sendKeys(password).build().perform();
         MyFunc.Wait(2);
 
         WebElement register=driver.findElement(By.cssSelector("[name='register-button']"));
-        new Actions(driver).moveToElement(register).click().build().perform();
+        actions.moveToElement(register).click().build().perform();
         MyFunc.Wait(2);
 
         WebElement confirmationMessage=driver.findElement(By.xpath("//div[@class='result']"));
