124a1a32292dc8982bdc96cae808ab7e4692d169
kayyamervee <kayaamervee01@gmail.com>
2024-09-16 23:12:15 +0300

    WebDriverWait eklendi.

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,12 +1,16 @@
 package Utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BaseDriver {
 
     public static WebDriver driver;
+    public static WebDriverWait wait;
+
     static {
+        wait=new WebDriverWait(driver,Duration.ofSeconds(20));
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
