3351983dafa75176cd3a878292af434ccc572b00
kayyamervee <kayaamervee01@gmail.com>
2024-09-16 17:52:47 +0300

    US_208 revize edilmiştir.

--- src/US_208.java
+++ src/US_208.java
@@ -14,50 +14,51 @@ public class US_208 extends BaseDriver {
 
         driver.get("https://demowebshop.tricentis.com/");
         MyFunc.Wait(2);
+        Actions actions=new Actions(driver);
 
         WebElement logIn=driver.findElement(By.cssSelector("li>a[href='/login' ]"));
-        new Actions(driver).click(logIn).build().perform();
+        actions.click(logIn).build().perform();
         MyFunc.Wait(2);
 
         String email="kayaamerve01@gmail.com";
         WebElement emailBox=driver.findElement(By.xpath("//div[@class='inputs' ]//input[@type='text']"));
-        new Actions(driver).moveToElement(emailBox).click().sendKeys(email).build().perform();
+        actions.moveToElement(emailBox).click().sendKeys(email).build().perform();
         MyFunc.Wait(2);
 
         String password="Team6merve";
         WebElement passwordBox=driver.findElement(By.cssSelector("div[class='inputs' ]>:nth-child(2)[type='password' ]"));
-        new Actions(driver).moveToElement(passwordBox).click().sendKeys(password).build().perform();
+        actions.moveToElement(passwordBox).click().sendKeys(password).build().perform();
         MyFunc.Wait(2);
 
         WebElement loginBox=driver.findElement(By.xpath("(//input[@type='submit' ])[2]"));
-        new Actions(driver).click(loginBox).build().perform();
+        actions.click(loginBox).build().perform();
         MyFunc.Wait(2);
 
         WebElement computersMenu=driver.findElement(By.xpath("(//a[@href='/computers' ])[1]"));
-        new Actions(driver).moveToElement(computersMenu).build().perform();
+        actions.moveToElement(computersMenu).build().perform();
         MyFunc.Wait(2);
 
         WebElement selectNotebooks=driver.findElement(By.xpath("(//a[@href='/notebooks' ])[1]"));
         MyFunc.Wait(2);
-        new Actions(driver).click(selectNotebooks).build().perform();
+        actions.click(selectNotebooks).build().perform();
         MyFunc.Wait(2);
 
-        new Actions(driver).scrollByAmount(0,300).build().perform();
+        actions.scrollByAmount(0,300).build().perform();
 
         WebElement addToCart=driver.findElement(By.xpath("//input[@value='Add to cart' ]"));
-        new Actions(driver).click(addToCart).build().perform();
+        actions.click(addToCart).build().perform();
         MyFunc.Wait(2);
 
-        new Actions(driver).scrollByAmount(300,0).build().perform();
+        actions.scrollByAmount(300,0).build().perform();
 
         WebElement shoppingCart=driver.findElement(By.xpath("(//a[@class='ico-cart' ])[1]"));
-        new Actions(driver).click(shoppingCart).build().perform();
+        actions.click(shoppingCart).build().perform();
         MyFunc.Wait(2);
 
-        new Actions(driver).scrollByAmount(0,300).build().perform();
+        actions.scrollByAmount(0,300).build().perform();
 
         WebElement applyCoupon=driver.findElement(By.cssSelector("input[value='Apply coupon' ]"));
-        new Actions(driver).click(applyCoupon).build().perform();
+        actions.click(applyCoupon).build().perform();
         MyFunc.Wait(2);
 
         WebElement couponText=driver.findElement(By.xpath("//div[@class='message' ]"));
@@ -66,7 +67,7 @@ public class US_208 extends BaseDriver {
         Assert.assertTrue("Uyarı mesajına ulaşılamadı!",couponText.getText().contains("The coupon code you entered couldn't be applied to your order"));
 
         WebElement addGiftCart=driver.findElement(By.xpath("//input[@value='Add gift card' ]"));
-        new Actions(driver).click(addGiftCart).build().perform();
+        actions.click(addGiftCart).build().perform();
         MyFunc.Wait(2);
 
         WebElement giftCartText=driver.findElement(By.xpath("//div[@class='message' ]"));
@@ -88,16 +89,16 @@ public class US_208 extends BaseDriver {
 
         String postalCode="10001";
         WebElement postalCodeBox=driver.findElement(By.xpath("//input[@class='zip-input' ]"));
-        new Actions(driver).moveToElement(postalCodeBox).click().sendKeys(postalCode).build().perform();
+        actions.moveToElement(postalCodeBox).click().sendKeys(postalCode).build().perform();
         MyFunc.Wait(2);
 
         WebElement acceptButton=driver.findElement(By.xpath("(//input[@type='checkbox' ])[2]"));
-        new Actions(driver).click(acceptButton).build().perform();
+        actions.click(acceptButton).build().perform();
         System.out.println("Kabul butonu seçilebilir durumda mı?"+acceptButton.isSelected());
         MyFunc.Wait(2);
 
         WebElement clickCheckout=driver.findElement(By.xpath("//button[@type='submit' ]"));
-        new Actions(driver).click(clickCheckout).build().perform();
+        actions.click(clickCheckout).build().perform();
         MyFunc.Wait(2);
 
         WebElement continueButtonFirst=driver.findElement(By.xpath("(//input[@title='Continue' ])[1]"));
@@ -127,12 +128,12 @@ public class US_208 extends BaseDriver {
 
         String name="Merve Kaya";
         WebElement cardName=driver.findElement(By.xpath("//input[@name='CardholderName' ]"));
-        new Actions(driver).moveToElement(cardName).click().sendKeys(name).build().perform();
+        actions.moveToElement(cardName).click().sendKeys(name).build().perform();
         MyFunc.Wait(2);
 
         String number="4242 4242 4242 4242";
         WebElement cardNumber=driver.findElement(By.xpath("//input[@id='CardNumber' ]"));
-        new Actions(driver).moveToElement(cardNumber).click().sendKeys(number).build().perform();
+        actions.moveToElement(cardNumber).click().sendKeys(number).build().perform();
         MyFunc.Wait(2);
 
         WebElement cardDateMonth=driver.findElement(By.xpath("//select[@id='ExpireMonth' ]"));
@@ -147,14 +148,14 @@ public class US_208 extends BaseDriver {
 
         String code="123";
         WebElement cardCode=driver.findElement(By.xpath("//input[@name='CardCode' ]"));
-        new Actions(driver).moveToElement(cardCode).click().sendKeys(code).build().perform();
+        actions.moveToElement(cardCode).click().sendKeys(code).build().perform();
         MyFunc.Wait(2);
 
         WebElement continueButtonFifth=driver.findElement(By.xpath("//input[@onclick='PaymentInfo.save()' ]"));
         continueButtonFifth.click();
         MyFunc.Wait(2);
 
-        new Actions(driver).scrollByAmount(0,300).build().perform();
+        actions.scrollByAmount(0,300).build().perform();
 
         WebElement confirmButton=driver.findElement(By.xpath("//input[@onclick='ConfirmOrder.save()' ]"));
         confirmButton.click();
@@ -165,7 +166,7 @@ public class US_208 extends BaseDriver {
         Assert.assertTrue("Mesaj görüntülenemedi!",confirmText.getText().contains("Your order has been successfully processed!"));
 
         WebElement logoutButton=driver.findElement(By.xpath("//a[text()='Log out' ]"));
-        new Actions(driver).click(logoutButton).build().perform();
+        actions.click(logoutButton).build().perform();
 
         tearDown();
     }
