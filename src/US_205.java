630bda059bf85baa2edbca43b5a1dd18fa850735
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-09-17 22:57:17 +0300

    US_205 emailyanlış password yanlış test ile emailboş password doğru olan testler eklendi

--- src/US_205.java
+++ src/US_205.java
@@ -65,7 +65,64 @@ public class US_205 extends BaseDriver {
 
         WebElement logInMassageControl=driver.findElement(By.xpath("//div[@class='message-error']//ul//li"));
         wait.until(ExpectedConditions.visibilityOf(logInMassageControl));
-        Assert.assertTrue("Mesaj görüntülenemedi",logInMassageControl.getText().contains("The credentials provided are incorrect"));
+        Assert.assertTrue("Mesaj görüntülenemedi",logInMassageControl.getText().contains("No customer account found"));
+
+        tearDown();
+    }
+
+    @Test
+    public void emailInvalidPasswordInvalid(){
+
+        driver.get("https://demowebshop.tricentis.com/");
+        wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
+
+        WebElement logIn=driver.findElement(By.cssSelector("[class='ico-login']"));
+        wait.until(ExpectedConditions.elementToBeClickable(logIn));
+        new Actions(driver).click(logIn).build().perform();
+
+        WebElement email=driver.findElement(By.cssSelector("[class='email']"));
+        wait.until(ExpectedConditions.visibilityOf(email));
+        email.sendKeys(invalidEmailStr);
+
+        WebElement password=driver.findElement(By.cssSelector("[class='password']"));
+        wait.until(ExpectedConditions.visibilityOf(password));
+        password.sendKeys(invalidPasswordStr);
+
+        WebElement logInClick=driver.findElement(By.xpath("(//div[@class='buttons'])[3]//input"));
+        wait.until(ExpectedConditions.elementToBeClickable(logInClick));
+        new Actions(driver).click(logInClick).build().perform();
+
+        WebElement logInMassageControl=driver.findElement(By.xpath("//div[@class='message-error']//ul//li"));
+        wait.until(ExpectedConditions.visibilityOf(logInMassageControl));
+        Assert.assertTrue("Mesaj görüntülenemedi",logInMassageControl.getText().contains("No customer account found"));
+
+        tearDown();
+    }
+
+    @Test
+    public void emailNullPasswordValid(){
+
+        driver.get("https://demowebshop.tricentis.com/");
+        wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
+
+        WebElement logIn=driver.findElement(By.cssSelector("[class='ico-login']"));
+        wait.until(ExpectedConditions.elementToBeClickable(logIn));
+        new Actions(driver).click(logIn).build().perform();
+
+        WebElement email=driver.findElement(By.cssSelector("[class='email']"));
+        wait.until(ExpectedConditions.visibilityOf(email));
+
+        WebElement password=driver.findElement(By.cssSelector("[class='password']"));
+        wait.until(ExpectedConditions.visibilityOf(password));
+        password.sendKeys(passwordStr);
+
+        WebElement logInClick=driver.findElement(By.xpath("(//div[@class='buttons'])[3]//input"));
+        wait.until(ExpectedConditions.elementToBeClickable(logInClick));
+        new Actions(driver).click(logInClick).build().perform();
+
+        WebElement logInMassageControl=driver.findElement(By.xpath("//div[@class='message-error']//ul//li"));
+        wait.until(ExpectedConditions.visibilityOf(logInMassageControl));
+        Assert.assertTrue("Mesaj görüntülenemedi",logInMassageControl.getText().contains("No customer account found"));
 
         tearDown();
     }
