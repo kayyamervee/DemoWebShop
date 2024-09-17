bdd3e08aa65f3455b39a78445c67decd32cdbb61
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-09-18 01:17:00 +0300

    US_204 ve US_205 ' de waitler eklendi

--- src/US_204.java
+++ src/US_204.java
@@ -18,22 +18,27 @@ public class US_204 extends BaseDriver {
         driver.get("https://demowebshop.tricentis.com/");
         wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='ico-login']")));
         WebElement logIn=driver.findElement(By.cssSelector("[class='ico-login']"));
         wait.until(ExpectedConditions.elementToBeClickable(logIn));
         new Actions(driver).click(logIn).build().perform();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='email']")));
         WebElement email=driver.findElement(By.cssSelector("[class='email']"));
         wait.until(ExpectedConditions.visibilityOf(email));
         email.sendKeys(emailStr);
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='password']")));
         WebElement password=driver.findElement(By.cssSelector("[class='password']"));
         wait.until(ExpectedConditions.visibilityOf(password));
         password.sendKeys(passwordStr);
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='buttons'])[3]//input")));
         WebElement logInClick=driver.findElement(By.xpath("(//div[@class='buttons'])[3]//input"));
         wait.until(ExpectedConditions.elementToBeClickable(logInClick));
         new Actions(driver).click(logInClick).build().perform();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@class='account'])[1]")));
         WebElement logInControl=driver.findElement(By.xpath("(//a[@class='account'])[1]"));
         Assert.assertTrue("Giriş başarısız...",logInControl.getText().contains("Auser"));
 
