a417c4f0b5036f004051dd27aa757720b636a009
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-09-18 00:30:46 +0300

    US_205 email doğru password boş olan testler eklendi

--- src/US_205.java
+++ src/US_205.java
@@ -109,12 +109,33 @@ public class US_205 extends BaseDriver {
         wait.until(ExpectedConditions.elementToBeClickable(logIn));
         new Actions(driver).click(logIn).build().perform();
 
-        WebElement email=driver.findElement(By.cssSelector("[class='email']"));
-        wait.until(ExpectedConditions.visibilityOf(email));
-
         WebElement password=driver.findElement(By.cssSelector("[class='password']"));
         wait.until(ExpectedConditions.visibilityOf(password));
         password.sendKeys(passwordStr);
+
+        WebElement logInClick=driver.findElement(By.xpath("(//div[@class='buttons'])[3]//input"));
+        wait.until(ExpectedConditions.elementToBeClickable(logInClick));
+        new Actions(driver).click(logInClick).build().perform();
+
+        WebElement logInMassageControl=driver.findElement(By.xpath("//div[@class='message-error']//ul//li"));
+        wait.until(ExpectedConditions.visibilityOf(logInMassageControl));
+        Assert.assertTrue("Mesaj görüntülenemedi",logInMassageControl.getText().contains("No customer account found"));
+
+        tearDown();
+    }
+
+    @Test
+    public void emailValidPasswordNull(){
+        driver.get("https://demowebshop.tricentis.com/");
+        wait.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
+
+        WebElement logIn=driver.findElement(By.cssSelector("[class='ico-login']"));
+        wait.until(ExpectedConditions.elementToBeClickable(logIn));
+        new Actions(driver).click(logIn).build().perform();
+
+        WebElement email=driver.findElement(By.cssSelector("[class='email']"));
+        wait.until(ExpectedConditions.visibilityOf(email));
+        email.sendKeys(emailStr);
 
         WebElement logInClick=driver.findElement(By.xpath("(//div[@class='buttons'])[3]//input"));
         wait.until(ExpectedConditions.elementToBeClickable(logInClick));
