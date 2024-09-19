3534e3ca937f8d65dcff5434abd39c1a2a2d742b
kayyamervee <kayaamervee01@gmail.com>
2024-09-20 00:07:17 +0300

    US_206 class adı değiştirildi.

--- src/US_206.java
+++ src/US_206.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
-public class US_206KM extends BaseDriver {
+public class US_206 extends BaseDriver {
     @Test
     public void Test1() {
         String emailStr="Auser"+(int)(Math.random()*100)+"@gmail.com";
